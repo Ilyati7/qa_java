b71ac451cf4c5037c7428e6455db2606556c7299
Ilya Titov <ilyati7@yandex.ru>
2025-10-19 23:58:39 +0300

    TestLion1

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -1,12 +1,14 @@
 package com.example;
 
+
 import java.util.List;
 
 public class Lion {
     boolean hasMane;
     private final Feline feline;
 
-    public Lion(String sex) throws Exception {
+    // Добавляем конструктор с параметром Feline
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -14,7 +16,7 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
-        this.feline = new Feline();
+        this.feline = feline;
     }
 
     public int getKittens() {
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,3 +1,5 @@
+
+
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Before;
@@ -7,45 +9,56 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
     @Mock
-    private Feline felineMock;
-
+    private Feline feline;
 
     private Lion lion;
 
     @Before
     public void setUp() throws Exception {
+        // Создаем экземпляр Lion с моком Feline
+        lion = new Lion("Самец", feline);
+    }
 
-        lion = new Lion("Самец");
+    // Изменим конструктор Lion для передачи Feline
+    public LionTest() {
+        // Конструктор без параметров для работы с Mockito
     }
 
     @Test
     public void testConstructorMale() throws Exception {
-        Lion lion = new Lion("Самец");
+        Lion lion = new Lion("Самец", feline);
         assertTrue(lion.doesHaveMane());
     }
 
     @Test
     public void testConstructorFemale() throws Exception {
-        Lion lion = new Lion("Самка");
+        Lion lion = new Lion("Самка", feline);
         assertFalse(lion.doesHaveMane());
     }
 
     @Test(expected = Exception.class)
     public void testConstructorInvalidSex() throws Exception {
-        new Lion("Неизвестный пол");
+        new Lion("Неизвестный пол", feline);
     }
 
     @Test
     public void testGetKittens() {
+        // Настраиваем мок для метода getKittens
+        when(feline.getKittens()).thenReturn(1);
         assertEquals(1, lion.getKittens());
     }
 
     @Test
     public void testGetFood() throws Exception {
+        // Настраиваем мок для метода getFood
+        when(feline.getFood("Хищник"))
+                .thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
         List<String> result = lion.getFood();
         assertEquals(List.of("Животные", "Птицы", "Рыба"), result);
     }
