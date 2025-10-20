7e97f1307bcfffb8059eb17ce924e408e1ff9d20
Ilya Titov <ilyati7@yandex.ru>
2025-10-20 22:09:22 +0300

    Извлёк лишние тесты

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,38 +1,11 @@
 import com.example.Animal;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.rules.ExpectedException;
-
-import java.util.List;
-
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
 public class AnimalTest {
     private Animal animal;
 
-    // Объявляем правило как @Rule
-    @Rule
-    public ExpectedException exceptionRule = ExpectedException.none();
-
-    @Parameterized.Parameters
-    public static Object[][] data() {
-        return new Object[][] {
-                {"Травоядное", List.of("Трава", "Различные растения")},
-                {"Хищник", List.of("Животные", "Птицы", "Рыба")}
-        };
-    }
-
-    private final String animalKind;
-    private final List<String> expectedResult;
-
-    public AnimalTest(String animalKind, List<String> expectedResult) {
-        this.animalKind = animalKind;
-        this.expectedResult = expectedResult;
-    }
 
     @Before
     public void setUp() {
@@ -40,23 +13,8 @@ public class AnimalTest {
     }
 
     @Test
-    public void shouldReturnCorrectFoodList() throws Exception {
-        List<String> result = animal.getFood(animalKind);
-        assertEquals("Списки должны совпадать", expectedResult, result);
-    }
-
-    @Test
     public void shouldReturnCorrectFamilyList() {
         String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
         assertEquals("Семейства должны совпадать", expected, animal.getFamily());
     }
-
-    @Test
-    public void shouldThrowExceptionForInvalidType() throws Exception {
-        exceptionRule.expect(Exception.class);
-        exceptionRule.expectMessage("Неизвестный вид животного, используйте значение Травоядное или Хищник");
-
-        animal.getFood("Неизвестный тип");
     }
-
-}
